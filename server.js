47de61ba6558f72cb2c0842cb4413583a0c745f9
alaayman <alaayman@gmail.com>
2021-08-02 18:34:41 +0200

    srever working ready for creating routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-app.use(cors);
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static("website"));
 
